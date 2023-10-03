9b649c660100d21b5bc1125edad952630a19942c
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-03 14:41:50 +0530

    2

---
+++ src/pages/RA/RA.js
@@ -0,0 +1,26 @@
+import classes from "./RA.module.css";
+import RaLists from "../RaDetail/RaLists";
+import EventCard from "../../components/EventCard/EventCard";
+import { useEffect } from "react";
+
+const RA = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  return (
+    <div className={classes.RA}>
+      {RaLists.map((list) => {
+        return (
+          <EventCard
+            key={list.id}
+            imgSrc={list.imgSrc}
+            heading={list.heading}
+            redirectLink={list.redirectLink}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default RA;

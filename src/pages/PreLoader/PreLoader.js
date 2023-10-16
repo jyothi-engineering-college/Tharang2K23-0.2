04032e9667d1fa8fba9a669c29eb5a8aedebb502
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-16 23:40:21 +0530

    loader

--- src/pages/PreLoader/PreLoader.js
+++ src/pages/PreLoader/PreLoader.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./preLoader.css";
 import Logo from "./Preloader.svg";
 
 function PreLoader() {
+  const [clickCount, setClickCount] = useState(0);
+  const [loadingCompleted, setLoadingCompleted] = useState(false);
+  const [hasClickedOnce, setHasClickedOnce] = useState(false);
+
+  const handleLogoClick = () => {
+    setClickCount(clickCount + 1);
+    setHasClickedOnce(true);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadingCompleted(true);
+    }, 10000);  // Adjust this time based on your actual loading time.
+
+    return () => clearTimeout(timer);  // Clean up timer on unmount
+  }, []);
+
   return (
     <div className="preLoader">
-      <img className="preLoaderLogo" src={Logo} alt="preloader" />
-      <h1 className="preLoaderheading">Karma 22</h1>
+      {loadingCompleted ? (
+        <div>Your score: {clickCount}</div>
+      ) : (
+        <>
+          <img className="preLoaderLogo" src={Logo} alt="preloader" onClick={handleLogoClick} />
+          <h1 className="preLoaderheading">Loading</h1>
+          {hasClickedOnce && <div className="scoreDisplay">Your score: {clickCount}</div>}
+        </>
+      )}
     </div>
   );
 }

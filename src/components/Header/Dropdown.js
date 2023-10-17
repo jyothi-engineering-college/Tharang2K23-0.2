6620a8560c95a32bf57260a336e747d3c4d5a6ec
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-17 14:52:55 +0530

    hello how are you

--- src/components/Header/Dropdown.js
+++ src/components/Header/Dropdown.js
@@ -4,7 +4,6 @@ import classes from "./Dropdown.module.css";
 const Dropdown = (props) => {
   return (
     <ul className={classes.dropdown}>
-      {/* <li className={classes.drops}><Link to="/events/cultural" onClick={props.click} className={classes.links}>Culturals</Link></li> */}
       <li className={classes.drops}><Link to="/events/AD" onClick={props.click} className={classes.links}>AD</Link></li>
       <li className={classes.drops}><Link to="/events/BSH" onClick={props.click} className={classes.links}>BSH</Link></li>
       <li className={classes.drops}><Link to="/events/CE" onClick={props.click} className={classes.links}>CE</Link></li>
@@ -14,10 +13,7 @@ const Dropdown = (props) => {
       <li className={classes.drops}><Link to="/events/ME" onClick={props.click} className={classes.links}>ME</Link></li>
       <li className={classes.drops}><Link to="/events/MR" onClick={props.click} className={classes.links}>MR</Link></li>
       <li className={classes.drops}><Link to="/events/RA" onClick={props.click} className={classes.links}>RA</Link></li>
-            {/* <li className={classes.drops}><Link to="/events/technical" onClick={props.click} className={classes.links}>Workshops</Link></li>
-            <li className={classes.drops}><Link to="/events/games" onClick={props.click} className={classes.links}>Games</Link></li>
-            <li className={classes.drops}><Link to="/events/hackathon" onClick={props.click} className={classes.links}>Hackathon</Link></li>
-            <li className={classes.drops}><Link to="/events/talk" onClick={props.click} className={classes.links}>Expert Talk</Link></li> */}
+            
     </ul>
   );
 };

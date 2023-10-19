ae0ffcc0959f11e2cc6c32b3a1d34d74b37bbe06
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-19 12:41:05 +0530

    footer need to fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,75 +2,26 @@ import classes from "./Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { HashLink } from "react-router-hash-link";
-import Logo from "./header_logo-2.png";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.row}>
-        <div className={`${classes.col} ${classes.centerVertically}`}>
-          <img className={classes.logo} src={Logo} alt="Tarang logo" />
-        </div>
-
-        <div className={classes.col}>
-          <h2>Contact us</h2>
-          <p>Jyothi Engineering College, Cheruthuruthy</p>
-          <p>Cheruthuruthy - P.O</p>
-          <p>Kerala 679531</p>
-          <p className={classes.email}>info@jecc.ac.in</p>
-          <p className={classes.contactNo}>04884 - 259000</p>
-          <p className={classes.contactNo}>04884 - 274423</p>
-        </div>
-
-        <div className={classes.col}>
-          <h2>Links</h2>
-          <ul className={classes.noBullets}>
-            {" "}
-            {/* Added class to remove bullets */}
-            <HashLink
-              to="/#about"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <li>About</li>
-            </HashLink>
-            <HashLink
-              to="/#contact"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <li>Contact us</li>
-            </HashLink>
-            <a
-              href="https://docs.google.com/document/d/1cJfjOhoEfW_JhiMF-tUJXNWwRKxSIN-KrIlJCnh0EZ4/edit?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <li>Privacy policy</li>
-            </a>
-          </ul>
-        </div>
-
-        <div className={classes.col}>
-          <h2>Follow us on</h2>
-          <div className={classes.socialIcons}>
-            <a
-              href="https://www.instagram.com/tharangjyothi/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon className={classes.icons} icon={faInstagram} />
-            </a>
-            <a
-              href="https://www.youtube.com/@JyothiEngineeringCollege"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon className={classes.icons} icon={faYoutube} />
-            </a>
-          </div>
-        </div>
+      
+      <div className={classes.socialIcons}>
+        <a href="https://www.instagram.com/tharangjyothi/" target="_blank" rel="noopener noreferrer" title="Did you know? Our Instagram has behind-the-scenes content!">
+          <FontAwesomeIcon className={classes.icons} icon={faInstagram} />
+        </a>
+        <a href="https://www.youtube.com/@JyothiEngineeringCollege" target="_blank" rel="noopener noreferrer" title="Trivia: Our first YouTube video reached 10K views in a week!">
+          <FontAwesomeIcon className={classes.icons} icon={faYoutube} />
+        </a>
       </div>
-      <hr className={classes.hr} />
+
+      <div className={classes.col}>
+        <HashLink to="/#about">About</HashLink>
+        <HashLink to="/#contact">Contact us</HashLink>
+        <a href="https://example.com/privacy" target="_blank" rel="noopener noreferrer">Privacy policy</a>
+      </div>
+
       <p className={classes.copyright}>
         Tharang'23 | Copyright &#169; All rights reserved.
       </p>

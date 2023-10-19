ebfa3c8a1d069a08d7ffc1ef43d17fe7c50200fa
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-19 14:50:48 +0530

    nav

--- src/pages/EventPage/EventPage.js
+++ src/pages/EventPage/EventPage.js
@@ -1,202 +1,29 @@
+import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import Background from "../../UI/Background";
 import classes from "./EventPage.module.css";
+import eventsData from "./data.json";
 
 const EventPage = () => {
   return (
     <section className={classes.eventPage}>
       <Background className={classes.bg}>
         <div className={classes.tabs}>
-           <div className={classes.tabList}>
-            {/*<div className={`${classes.tabHead}`}>
-              <NavLink
-                to="cultural"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Cultural
-              </NavLink>
-            </div> */}
-            {/* <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="technical"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Workshops
-              </NavLink>
-            </div> */}
-            {/* <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="games"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Games
-              </NavLink>
-            </div> */}
-            {/* <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="hackathon"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Hackathon
-              </NavLink>
-            </div> */}
-            {/* <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="talk"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Expert Talk
-              </NavLink>
-            </div> */}
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="AD"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                AD
-              </NavLink>
-            </div>
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="BSH"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                BSH
-              </NavLink>
-            </div>
-            
-            
-              
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="CSE"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                CSE
-              </NavLink>
-            </div>
-
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="CE"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                CE
-              </NavLink>
-            </div>
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="ECE"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                ECE
-              </NavLink>
-            </div>
-
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="EEE"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                EEE
-              </NavLink>
-            </div>
-
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="ME"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                ME
-              </NavLink>
-            </div>
-
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="MR"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                MR
-              </NavLink>
-            </div>
-
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="RA"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                RA
-              </NavLink>
-            </div>
-            <div className={`${classes.tabHead}`}>
-              <NavLink
-                to="technical"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.active} ${classes.links}`
-                    : `${classes.links}`
-                }
-              >
-                Technical
-              </NavLink>
-            </div>
-
+          <div className={classes.tabList}>
+            {eventsData.map((event, index) => (
+              <div key={index} className={classes.tabHead}>
+                <NavLink
+                  to={event.path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${classes.active} ${classes.links}`
+                      : `${classes.links}`
+                  }
+                >
+                  {event.name}
+                </NavLink>
+              </div>
+            ))}
           </div>
         </div>
         <Outlet />

89ebf0489d7ff202e6dc4d009c1d5dbceb5c3da6
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-19 11:06:36 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import classes from "./Header.module.css";
-
 import { FiMenu } from "react-icons/fi";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
-
+import { faArrowDown, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { HashLink as Link } from "react-router-hash-link";
 import Dropdown from "./Dropdown";
-import { HashLink } from "react-router-hash-link";
 import Logo from "./header_logo.jpg";
 
 const Scroll = require("react-scroll");
+const Drop = Scroll.Link;
 
 const Header = () => {
-  const Drop = Scroll.Link;
   const [scrolled, isScrolled] = useState(false);
   const [mobile, setMobile] = useState(false);
   const [drop, setDrop] = useState(false);
@@ -25,9 +21,7 @@ const Header = () => {
   };
 
   return (
-    <header
-      className={`${!scrolled ? classes.header : classes.scrolledHeader}`}
-    >
+    <header className={`${!scrolled ? classes.header : classes.scrolledHeader}`}>
       <div className={classes.elements}>
         <div className={classes.logobox}>
           <Drop
@@ -43,182 +37,81 @@ const Header = () => {
             </Link>
           </Drop>
         </div>
-
         <div className={classes.navbox}>
           <ul className={classes.nav}>
             <li className={classes.navLink}>
-              <Drop
-                activeClass={classes.active}
-                to="home"
-                spy="true"
-                smooth={true}
-                offset={-100}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/#home">
-                  Home
-                </HashLink>
+              <Drop to="home" spy="true" smooth={true} offset={-100} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/#home">Home</Link>
               </Drop>
             </li>
-            <li className={classes.navLink}>
-              <Drop
-                activeClass={classes.active}
-                to="about"
-                spy="true"
-                smooth={true}
-                offset={-120}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/home#about">
-                  About
-                </HashLink>
-              </Drop>
-            </li>
-            <li
-              className={`${classes.dropLink}`}
-              onClick={() => setDrop(!drop)}
-            >
+            <li className={`${classes.dropLink}`} onClick={() => setDrop(!drop)}>
               {drop ? <Dropdown click={() => setDrop(!drop)} /> : ""}Events
-              <FontAwesomeIcon
-                size="sm"
-                style={{
-                  display: "inline-block",
-                  marginLeft: ".3rem",
-                  color: "white",
-                }}
-                icon={faArrowDown}
-                fade
-              />
+              <FontAwesomeIcon size="sm" style={{ display: "inline-block", marginLeft: ".3rem", color: "white" }} icon={faArrowDown} fade />
             </li>
-            {/* <li className={classes.navLink}>
-              <Drop
-                activeClass={classes.active}
-                to="ambassador"
-                spy="true"
-                smooth={true}
-                offset={-120}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/home#ambassador">
-                  Sponsors
-                </HashLink>
-              </Drop>
-            </li> */}
             <li className={classes.navLink}>
-              <Drop
-                activeClass={classes.active}
-                to="contact"
-                spy="true"
-                smooth={true}
-                offset={-120}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/home#contact">
-                  Contact
-                </HashLink>
+              <Drop to="about" spy="true" smooth={true} offset={-120} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/home#about">About</Link>
+              </Drop>
+            </li>
+            
+            <li className={classes.navLink}>
+              <Drop to="contact" spy="true" smooth={true} offset={-120} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/home#contact">Contact</Link>
+              </Drop>
+            </li>
+            <li className={classes.navLink}>
+              <Drop to="contact" spy="true" smooth={true} offset={-120} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link
+                  className={classes.homeLink}
+                  to="/"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://tharang23.netlify.app/", "_blank");
+                  }}
+                >
+                  Campus Ambassador
+                </Link>
               </Drop>
             </li>
           </ul>
-
-          {/* <Link target="_blank" rel="noopener noreferrer" to="/tickets">
-            <button className={classes.btn}>Buy Tickets</button>
-          </Link> */}
-
           <div className={classes.hamburger} onClick={() => setMobile(!mobile)}>
-            {mobile ? (
-              <FontAwesomeIcon
-                icon={faXmark}
-                style={{ color: "white" }}
-                size="3x"
-              />
-            ) : (
-              <FiMenu size={20} />
-            )}
+            {mobile ? <FontAwesomeIcon icon={faXmark} style={{ color: "white" }} size="3x" /> : <FiMenu size={20} />}
           </div>
         </div>
-
         <div className={`${!mobile ? classes.none : classes.mobileBox}`}>
-          {/* <Link target="_blank" rel="noopener noreferrer" to="/tickets">
-            <button className={classes.btn1}>Buy Tickets</button>
-          </Link> */}
-
-          {/* Mobile Page */}
-
           <ul className={classes.mobileNav}>
             <li>
-              <Drop
-                onClick={() => setMobile(!mobile)}
-                activeClass={classes.active}
-                to="home"
-                spy="true"
-                smooth={true}
-                offset={-120}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/#home">
-                  Home
-                </HashLink>
-              </Drop>
-            </li>
-            <li>
-              <Drop
-                onClick={() => setMobile(!mobile)}
-                activeClass={classes.active}
-                to="about"
-                spy="true"
-                smooth={true}
-                offset={-100}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/home#about">
-                  About
-                </HashLink>
+              <Drop onClick={() => setMobile(!mobile)} to="home" spy="true" smooth={true} offset={-120} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/#home">Home</Link>
               </Drop>
             </li>
             <li className={`${classes.mobLink}`} onClick={() => setDrop(!drop)}>
-              {drop ? (
-                <Dropdown
-                  click={() => {
-                    setDrop(!drop);
-                    setMobile(!mobile);
-                  }}
-                />
-              ) : (
-                ""
-              )}
-              Events
-              <FontAwesomeIcon
-                size="sm"
-                style={{
-                  display: "inline-block",
-                  marginLeft: ".3rem",
-                  color: "white",
-                }}
-                icon={faArrowDown}
-                fade
-              />
+              {drop ? <Dropdown click={() => { setDrop(!drop); setMobile(!mobile); }} /> : ""}Events
+              <FontAwesomeIcon size="sm" style={{ display: "inline-block", marginLeft: ".3rem", color: "white" }} icon={faArrowDown} fade />
             </li>
-           
             <li>
-              <Drop
-                onClick={() => setMobile(!mobile)}
-                activeClass={classes.active}
-                to="contact"
-                spy="true"
-                smooth={true}
-                offset={-100}
-                duration={600}
-                style={{ color: "white", textDecoration: "none" }}
-              >
-                <HashLink className={classes.homeLink} to="/home#contact">
-                  Contact
-                </HashLink>
+              <Drop onClick={() => setMobile(!mobile)} to="about" spy="true" smooth={true} offset={-100} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/home#about">About</Link>
+              </Drop>
+            </li>
+            
+            <li>
+              <Drop onClick={() => setMobile(!mobile)} to="contact" spy="true" smooth={true} offset={-100} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link className={classes.homeLink} to="/home#contact">Contact</Link>
+              </Drop>
+            </li>
+            <li>
+              <Drop onClick={() => setMobile(!mobile)} to="contact" spy="true" smooth={true} offset={-100} duration={600} style={{ color: "white", textDecoration: "none" }}>
+                <Link
+                  className={classes.homeLink}
+                  to="/"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://tharang23.netlify.app/", "_blank");
+                  }}
+                >
+                  Campus Ambassador
+                </Link>
               </Drop>
             </li>
           </ul>

71ec657809794663ed4a56d44619582176fd2e94
Alan Jose Santo <realityaudiostudio@gmail.com>
2023-10-22 11:54:00 +0530

    added redirection

--- src/pages/CulturalDetail/CulturalLists.js
+++ src/pages/CulturalDetail/CulturalLists.js
@@ -7,7 +7,7 @@ const CulturalLists = [
       desc: "Get ready to be swept away into a world of enchanting melodies! Virtuos Music Band is back for Tharang 2.0 with 'The Legato' – an interactive musical retreat that promises pure magic.Stay tuned for an unforgettable musical experience! 🎵✨" ,
       regFee: "Fee",
       contact: "Alan Jose Santo , Phone number  8086350450",
-      redirectLink: "",
+      redirectLink: "https://virtuosi.netlify.app/",
   }
 ]
 export default CulturalLists;

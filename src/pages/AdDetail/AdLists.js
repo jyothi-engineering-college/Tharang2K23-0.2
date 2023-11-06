35f272fe3fe42c51bced314c76566588bea5a70f
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-06 15:06:08 +0530

    cse

--- src/pages/AdDetail/AdLists.js
+++ src/pages/AdDetail/AdLists.js
@@ -1,28 +1,56 @@
 const AdLists = [
   {
-    id: 1,
-    name: "REVXPO",
-    imgSrc: "/assets/ME/show.jpg",
-    heading: "REVXPO",
-    rule1: "Flagship Event of Mechanical Department",
-    rule2: "Exclusive vehicles from all over Kerala",
-    rule3: "Treat your ears to the music of engines",
-    regFee: "50rs per Ticket",
-    contact: "Dobin: 79073 90580",
-    redirectLink: `https://bit.ly/3FKKaUc`
+    id: 3,
+    name: "AI/Metaverse Workshop",
+    imgSrc: "/assets/AD/a11.png",
+    heading: "AI/Metaverse Workshop",
+    rule1: "Date: October 28, 2023",
+    rule2: "Time: 9:30 AM - 12:30 PM",
+    rule3: "Venue: Decennial Hall, Jyothi Engineering College, Thrissur",
+    rule4: "Topic: AI/Metaverse",
+    rule5: "Sponsored by: Unique World Robotics",
+    rule6: "In collaboration with: AIDA, CSI, and IEDC",
+    rule7: "Type: Pre-Tharang event",
+    regFee: "150",
+    contact: "Chandini T S",
+    redirectLink: "https://forms.gle/CZmcLxGjCCzmtrZx6"
   },
   {
-    id: 2,
-    name: "MAAYA",
-    imgSrc: "/assets/ME/maya.png",
-    heading: "MAAYA",
-    rule1: "A vibe that can't be described",
-    rule2: "A play of light, art, illusion and sound",
-    rule3: "Elevate the soul of the viewer",
-    regFee: "Free",
-    contact: "SHINE TS: 8137942347",
-    redirectLink: `/events/MeDetail/MAAYA`
+    id: 4,
+    name: "IntruderQuest",
+    imgSrc: "/assets/AD/a12.jpg",
+    heading: "Intruder's Quest",
+    rule1: "An exciting part of the big Razzle Dazzle event.",
+    rule2: "Navigate a tricky maze filled with lasers.",
+    rule3: "Find the hidden treasure to win.",
+    regFee: "70",
+    contact: "Josten S Cheeran",
+    redirectLink: `/events/AdDetail/IntruderQuest`
   },
-  // ... (other entries)
-]
+  {
+    id: 5,
+    name: "Blind",
+    imgSrc: "/assets/AD/a13.jpg",
+    heading: "Blind's Man Bluff",
+    rule1: "A part of the Razzle Dazzle event challenging your senses.",
+    rule2: "A unique form of hide-and-seek.",
+    rule3: "Use your hearing to tag others.",
+    regFee: "20 per head",
+    contact: "Surya Narayan",
+    redirectLink: `/events/AdDetail/Blind`
+  },
+  {
+    id: 6,
+    name: "PhotoBooth",
+    imgSrc: "/assets/AD/a14.jpg",
+    heading: "Photo Booth",
+    rule1: "Have your vintage-style photographs taken.",
+    rule2: "Capture memories at the event.",
+    rule3: "Available on Nov 10 & 11.",
+    regFee: "50",
+    contact: "Josten S Cheeran",
+    redirectLink:`/events/AdDetail/PhotoBooth`
+  }
+];
+
 export default AdLists;
--- src/pages/CseDetail/CseLists.js
+++ src/pages/CseDetail/CseLists.js
@@ -8,6 +8,18 @@ const AdLists = [
       regFee: "free for all",
       contact: "Sanjon Rion - 9995038572",
       redirectLink: "https://tinyurl.com/9pdrhdac",
-  }
+  },
+  {
+    id: 2,
+    name: "LaserTag",
+    imgSrc: "/assets/CSE/lasertag.jpg",
+    heading: "Laser Tag",
+    desc: "Laser tag is a combat simulation using infrared guns.Players navigate arenas,tagging rivals with lasers. Without projectiles,it's safe for all. It promotes teamwork,strategy,and blends physical activity with fun." ,
+    regFee: "50 per head",
+    contact: "Sanjon Rion - 9995038572",
+    redirectLink: `/events/CseDetail/LaserTag`,
+},
+
+  
 ]
 export default AdLists;

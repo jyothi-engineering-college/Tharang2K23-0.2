402974755f7ea86945c7291d3d7031c175483c6b
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-07 11:25:50 +0530

    cse

--- src/pages/CseDetail/CseLists.js
+++ src/pages/CseDetail/CseLists.js
@@ -18,7 +18,51 @@ const AdLists = [
     regFee: "50 per head",
     contact: "Mohammed Irfan - 9061335998",
     redirectLink: `/events/CseDetail/LaserTag`,
-},
+},{
+  id: 3,
+  name: "InTheLoop",
+  imgSrc: "/assets/CSE/L.jpg",
+  heading: "In The Loop",
+  desc: "In the Loop - Coding Competition",
+  rule1: "Round I - Bug Hunt : Join the initial round for debugging challenges and output prediction. Solve the first debugging question to advance to the next phase.",
+  rule2: "Round II - Coding Questions In this round, face timed coding challenges on HackerRank. Use Java, C, C++, Python, or JavaScript. Win prizes for top performance.",
+  rule3: "Solo and Duo Participation. \n Open to undergraduates and lower educational levels.\nTop 10 teams advance to Round II.\nHeld as a Part of Jyothi Engineering College Tech Fest - Tharang2.0.\nHosted by the Computer Science Department at EAB205 and EAB109, Day 1 on November 10th at 10:30 AM.\n\nJoin the competition, test your coding skills, and compete for prizes and recognition. Entry fee: ₹50 for solo, ₹100 for duos. Good luck to all participants!",
+  regFee: "₹50 for solo, ₹100 for duos",
+  contact: "Muhammed Muhnis +91 89435 05858",
+  redirectLink: "https://forms.gle/zYfgf9rfynF8i1CF6"
+}
+,{
+  id: 4,
+  name: "Marshall",
+  imgSrc: "/assets/CSE/M.jpeg",
+  heading: "Marshall",
+  desc: "Calling all music enthusiasts! Join us for an unforgettable evening of Karaoke in a rustic bar-themed setting at Jyothi Engineering College, hosted by the Computer Science Department on November 10th. ",
+  rule1: "Get ready to immerse yourself in the warm, cozy atmosphere of our venue, adorned with wooden accents and dimmed lighting, creating the perfect backdrop for your vocal talents to shine. Whether you're a seasoned performer or just looking to have a blast, this event is the ideal platform to showcase your singing prowess. So, mark your calendars and let your inner rockstar shine on this unique night of music and merriment!",
+  regFee: "Free for all",
+  contact: "Alen shanto  90722 99940",
+  redirectLink: `/events/CseDetail/Marshall`,
+},{
+  id: 5,
+  name: "ReadyPlayerOne",
+  imgSrc: "/assets/CSE/RPO.png",
+  heading: "Laser Tag",
+  desc: "🎮 Get ready to embark on a gaming adventure like no other! Join us for 'Ready Player One' on November 10th and 11th, hosted by the Computer Science Department at Jyothi Engineering College. 🕹️👾",
+  rule1: "🌟 Immerse yourself in the virtual world, where gamers unite to showcase their skills, meet fellow players, and win exciting prizes.",
+   rule2: "🌟 From intense esports tournaments to casual gaming sessions, this event has it all. Don't miss the chance to be part of the ultimate gaming experience!",  
+    rule3: "🗓️ Save the date, spread the word, and let's game on! See you there! 🎮✨" ,
+  regFee: "30 per head",
+  contact: "Shamil 9633230926",
+  redirectLink: `/events/CseDetail/ReadyPlayerOne`,
+},{
+  id: 6,
+  name: "Khwaab",
+  imgSrc: "/assets/CSE/SING.png",
+  heading: "Khwaab",
+  desc: "Musical performance by artist mubaasco",
+  regFee: "Free for all",
+  contact: "Hisham N, Nikhil Job Mathew ",
+  redirectLink: `/events/CseDetail/Khwaab`,
+}
 
   
 ]

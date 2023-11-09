e119b89ed34ad286eba6873110d844acc11fe609
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-11-09 14:32:42 +0530

    time

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
    let interval = useRef();
 
    const startTimer = () => {
-     const countdownDate = new Date("Nov 9 2023 08:00:00").getTime();
+     const countdownDate = new Date("Nov 10 2023 08:00:00").getTime();
 
      interval = setInterval(() => {
        const now = new Date().getTime();
